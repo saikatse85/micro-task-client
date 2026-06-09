9ed04ede5c95e420e363e226608b32d45030fdca
saikatse85 <saikatse@gmail.com>
2026-06-09 21:26:58 +0600

    added redirectproblem

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,23 @@ const nextConfig = {
       { protocol: 'https', hostname: 'images.unsplash.com' },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'unsafe-none',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
